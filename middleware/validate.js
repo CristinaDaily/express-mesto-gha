226d72c09cf08718694f626737759640038f5ef5
Cristina <kristi.chichkanova@gmail.com>
2023-10-17 14:32:34 +0200

    validate avatar for new user

--- middleware/validate.js
+++ middleware/validate.js
@@ -6,7 +6,7 @@ const validateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().regex(avatarRegex),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
