a883ad9dce5368a1ecaafbd0f94cd6f72e523c1d
Cristina <kristi.chichkanova@gmail.com>
2023-10-17 14:41:28 +0200

    card link validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -47,7 +47,7 @@ const validateLoginData = celebrate({
 const validateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().uri().required(),
+    link: Joi.string().regex(avatarRegex).required(),
   }),
 });
 
